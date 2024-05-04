e91510283dd33023099d1478e80d6817e63690eb
JBostroem96 <Jbostroem20@outlook.com>
2024-05-03 22:28:25 -0500

    Refactored the search results, removing redundant code

---
+++ src/main/java/edu/controller/Search.java
@@ -0,0 +1,70 @@
+package edu.controller;
+
+import edu.matc.entity.Race;
+import edu.matc.entity.Team;
+import edu.matc.persistence.GenericDao;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+/**
+ * This class' purpose is to set the attribute depending on
+ * what option the user selects
+ */
+@WebServlet(
+        urlPatterns = {"/search"}
+)
+
+public class Search extends HttpServlet {
+
+    /**
+     * This method's purpose is to search for a race or all races
+     * @param req the request object
+     * @param resp the response object
+     * @throws ServletException the servlet exception object
+     * @throws IOException the io exception object
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        GenericDao raceDao = new GenericDao(Race.class);
+        GenericDao teamDao = new GenericDao(Team.class);
+
+        if (req.getParameter("submit").equals("search")) {
+
+            if (req.getParameter("searchType").equals("length")) {
+
+                req.setAttribute("races", raceDao.findByPropertyEqual("length", req.getParameter("searchTerm")));
+
+            } else if (req.getParameter("searchType").equals("division")) {
+
+                req.setAttribute("teams", teamDao.findByPropertyEqual("division", req.getParameter("searchTerm")));
+
+            } else if (req.getParameter("searchType").equals("raceName")) {
+
+                req.setAttribute("races", raceDao.findByPropertyEqual("name", req.getParameter("searchTerm")));
+
+            } else if (req.getParameter("searchType").equals("teamName")) {
+
+                req.setAttribute("teams", teamDao.findByPropertyEqual("name", req.getParameter("searchTerm")));
+
+            }
+
+        } else if (req.getParameter("submit").equals("viewAllRaces"))  {
+
+            req.setAttribute("races", raceDao.getAll());
+
+        } else {
+
+            req.setAttribute("teams", teamDao.getAll());
+        }
+
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/searchResults.jsp");
+        dispatcher.forward(req, resp);
+    }
+}
--- src/main/webapp/searchRace.jsp
+++ src/main/webapp/searchRace.jsp
@@ -8,13 +8,13 @@
 <div class="container bg-white">
     <h1 class="mt-5 text-center text-black fw-bold mb-5"><em>Search Race</em></h1>
     <div class="d-flex flex-column gap-5 fw-bold">
-        <form action="searchRace" class="form-inline">
+        <form action="search" class="form-inline">
                     <div class="search text-white p-5 border rounded" id="search_form">
                         <label class="mb-2" for="searchTerm">Search</label>
                         <input  class="form-control" type="search" name="searchTerm" id="searchTerm">
                         <div class="form-group">
                             <label for="name">Name</label>
-                            <input class="form-check-input" type="radio" name="searchType" id="name" value="name" checked>
+                            <input class="form-check-input" type="radio" name="searchType" id="name" value="raceName" checked>
                         </div>
                         <div class="form-group">
                             <label for="length">Length</label>
@@ -23,7 +23,7 @@
                     </div>
                 <div class="buttons mt-3">
                     <button type="submit" name="submit" class="btn btn-primary bg-success" value="search">Search</button>
-                    <button type="submit" name="submit" class="btn btn-primary bg-primary" value="viewAll">View All</button>
+                    <button type="submit" name="submit" class="btn btn-primary bg-primary" value="viewAllRaces">View All</button>
                     <button type="reset" name="clear" class="btn btn-primary bg-danger" value="clear">Clear</button>
                 </div>
         </form>
