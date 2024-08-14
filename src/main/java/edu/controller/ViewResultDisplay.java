611faa86756dc1f5a2f69b0aba6ce8864654dfec
JBostroem96 <Jbostroem20@outlook.com>
2024-08-14 11:49:02 -0500

    Reworked the division ranking calculation, removing an unnecessary loop and putting all the division rankings into an array instead of declaring them individually

--- src/main/java/edu/controller/ViewResultDisplay.java
+++ src/main/java/edu/controller/ViewResultDisplay.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.*;
 
 
@@ -83,19 +84,9 @@ public class ViewResultDisplay extends HttpServlet {
      */
     public List<TeamRace> rankDivision(List<TeamRace> teamRaces) {
 
-        int overallPlace = 0;
-        int maleDivision = 0;
-        int femaleDivision = 0;
-        int soloFemaleDivision = 0;
-        int soloMaleDivision = 0;
-        int mixedDivision = 0;
+        int[] divisions = {0, 0, 0, 0, 0, 0};
 
-        for (Division division : Division.values()) {
-
-            loopThrough(teamRaces, overallPlace,
-                    maleDivision, femaleDivision, mixedDivision,
-                    soloFemaleDivision, soloMaleDivision);
-        }
+        loopThrough(teamRaces, divisions);
 
         return teamRaces;
     }
@@ -103,51 +94,44 @@ public class ViewResultDisplay extends HttpServlet {
     /**
      * This method's purpose is to loop through the results and display the ranks
      * @param teamRaces the list of results
-     * @param overallPlace the overall rank
-     * @param maleDivision the male category
-     * @param femaleDivision the female category
-     * @param mixedDivision the mixed category
-     * @param soloFemaleDivision the solo female category
-     * @param soloMaleDivision the solo male category
+     * @param divisions the different division rankings
      */
-    public void loopThrough(List<TeamRace> teamRaces, int overallPlace, int maleDivision,
-                            int femaleDivision, int mixedDivision,
-                            int soloFemaleDivision, int soloMaleDivision) {
+    public void loopThrough(List<TeamRace> teamRaces, int[] divisions) {
 
         for (TeamRace entry : teamRaces) {
 
             switch (entry.getTeam().getCategory().getDivision()) {
                 case MALE:
 
-                    maleDivision++;
-                    entry.setDivisionPlace(maleDivision);
+                    divisions[0]++;
+                    entry.setDivisionPlace(divisions[0]);
 
                     break;
                 case FEMALE:
 
-                    femaleDivision++;
-                    entry.setDivisionPlace(femaleDivision);
+                    divisions[1]++;
+                    entry.setDivisionPlace(divisions[1]);
 
                     break;
                 case SOLO_MALE:
 
-                    soloMaleDivision++;
-                    entry.setDivisionPlace(soloMaleDivision);
+                    divisions[2]++;
+                    entry.setDivisionPlace(divisions[2]);
 
                     break;
                 case SOLO_FEMALE:
 
-                    soloFemaleDivision++;
-                    entry.setDivisionPlace(soloFemaleDivision);
+                    divisions[3]++;
+                    entry.setDivisionPlace(divisions[3]);
                     break;
                 case MIXED:
 
-                    mixedDivision++;
-                    entry.setDivisionPlace(mixedDivision);
+                    divisions[4]++;
+                    entry.setDivisionPlace(divisions[4]);
                     break;
             }
-            overallPlace++;
-            entry.setOverallPlace(overallPlace);
+            divisions[5]++;
+            entry.setOverallPlace(divisions[5]);
         }
     }
 }
