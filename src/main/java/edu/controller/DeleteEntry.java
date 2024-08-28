0d40e00b7c31ce0b6598c35244007d887a5ca6ce
JBostroem96 <Jbostroem20@outlook.com>
2024-08-28 12:31:46 -0500

    Added some documentation

--- src/main/java/edu/controller/DeleteEntry.java
+++ src/main/java/edu/controller/DeleteEntry.java
@@ -6,9 +6,18 @@ import edu.matc.util.UseLogger;
 import org.apache.logging.log4j.Logger;
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * This class' purpose is to delete the entry
+ * @param <T> the entity
+ */
 public class DeleteEntry<T> implements UseLogger {
 
 
+    /**
+     * This method's purpose is to delete the entry
+     * @param dao the generic dao
+     * @param req the request object
+     */
     public DeleteEntry(GenericDao<T> dao, HttpServletRequest req) {
 
         final Logger logger = log();
