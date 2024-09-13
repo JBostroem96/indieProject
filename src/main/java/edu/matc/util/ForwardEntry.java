a588b056232ee47c4793f949dd221ed9e7c94229
JBostroem96 <Jbostroem20@outlook.com>
2024-09-13 11:50:56 -0500

    Added documentation to the ForwardEntry utility class

--- src/main/java/edu/matc/util/ForwardEntry.java
+++ src/main/java/edu/matc/util/ForwardEntry.java
@@ -7,8 +7,23 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * This class' purpose is to forward the entry to the page
+ * @param <T> the first entity
+ * @param <U> the second entity
+ */
 public class ForwardEntry<T, U>  {
 
+    /**
+     * This method's purpose is to forward to the page
+     * @param url the url to forward to
+     * @param req the request object
+     * @param resp the response object
+     * @param entry the entry passed over
+     * @param displayEntries the list of entries to be displayed
+     * @throws ServletException if there is an issue in the servlet
+     * @throws IOException if the IO operations fail
+     */
     public ForwardEntry(String url, HttpServletRequest req, HttpServletResponse resp, T entry, List<U> displayEntries) throws ServletException, IOException {
 
         if (entry != null) {
